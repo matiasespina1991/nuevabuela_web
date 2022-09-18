da870353d5306387724e98695827ba6559e6bdbe
Matias Espina <matiasespina1991@gmail.com>
2022-09-19 01:29:35 +0200

    .

--- pages/inspirations/index.js
+++ pages/inspirations/index.js
@@ -102,17 +102,6 @@ function srcset(image, size, rows = 1, cols = 1) {
                     </ImageListItem>
                 ))}
                 </ImageList>
-
-                {
-                        selectedImage && selectedImage != '' &&
-                        <div className="modular" onClick={() => setSelectedImage('')}>
-                            <div className="close-modular" onClick={() => setSelectedImage('')}>X</div>
-                            <div className="modular-image-container">
-                                <img className="modular-image" src={selectedImage} alt="" />
-                            </div>
-                        </div>
-                    }
-
                 </main>
                 <Footer />
             </div>
